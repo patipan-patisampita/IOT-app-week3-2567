620653743aa4a675a2c5dc0c084bbcf86961d576
patipan-patisampita <patipan-patisampita@gmail.com>
2024-10-31 11:59:15 +0700

    update

---
+++ models/postModel.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+const postSchema = new Schema({
+    title: String,
+    slug: String,
+    content: String,
+    image: String,
+    CreatedAt: {
+        type: Date,
+        default: new Date(),
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    }
+})
+
+const Post = model('Post',postSchema)
+
+export default Post
